2f7beb6a844618b0499aa8e089e0dafa1597ea00
SamuraiT <t.yasukawa01@gmail.com>
2022-01-25 23:05:25 +0900

    initial setup for dynamic nft

---
+++ contracts/DynamicNFT.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract DynamicNFT {
+    constructor() {
+        console.log("dynamic nft");
+    }
+}
--- scripts/sample-script.js
+++ scripts/sample-script.js
@@ -5,7 +5,7 @@
 // Runtime Environment's members available in the global scope.
 const hre = require("hardhat");
 
-async function main() {
+const main = async () => {
   // Hardhat always runs the compile task when running scripts with its command
   // line interface.
   //
@@ -14,19 +14,20 @@ async function main() {
   // await hre.run('compile');
 
   // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const DynamicNFT = await hre.ethers.getContractFactory("DynamicNFT");
+  const dnft = await DynamicNFT.deploy();
 
-  await greeter.deployed();
+  await dnft.deployed();
 
-  console.log("Greeter deployed to:", greeter.address);
+  console.log("Greeter deployed to:", dnft.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main()
-  .then(() => process.exit(0))
-  .catch((error) => {
+(async () => {
+  try {
+    await main()
+    process.exit(0)
+  } catch (e) {
     console.error(error);
     process.exit(1);
-  });
+  }
+})()

d18aca3564582b4d8d7042c7a9fa4354a4d945f5
SamuraiT <t.yasukawa01@gmail.com>
2022-01-29 14:39:08 +0900

    Add and moved utils here
    Signed-off-by: SamuraiT <t.yasukawa01@gmail.com>

---
+++ components/utils.js
@@ -0,0 +1,22 @@
+const base64 = (str) => {
+  return btoa(unescape(encodeURIComponent( str )))
+}
+const ShowSVGImage = ({dataURI}) => {
+  const json = atob(dataURI.substring(29));
+  const { image } = JSON.parse(json);
+  console.log(image)
+  return (
+    <img src={image} />
+  )
+}
+
+const baseSvgSrc = (text, backgroundColor, textColor) => {
+  const baseSvg = `<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'> <style>.base { fill: ${textColor}; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='${backgroundColor}' /><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>${text}</text></svg>`
+  return `data:image/svg+xml;base64,${base64(baseSvg)}`
+}
+
+export {
+  base64,
+  ShowSVGImage,
+  baseSvgSrc
+}
